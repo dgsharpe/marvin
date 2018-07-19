bff1eae5be7276fe8d4991abf6a9996be3454df6
Gabriel Revells <gcrevell@mtu.edu>
2018-07-19 13:47:13 -0400

    Create separate functions to send emails/pushovers
    Move the logic to send email notifications or pushover notifications
    into separate functions

--- notifications.py
+++ notifications.py
@@ -4,6 +4,8 @@ import threading
 import time
 import notificationevent
 
+import http.client, urllib
+
 
 class Notifications(threading.Thread):
     def __init__(self, config, event_queue):
@@ -40,6 +42,31 @@ class Notifications(threading.Thread):
             else:
                 fail_event = notificationevent.NotificationEventType.EMAIL_FAILURE
                 self.eventQueue.put(notificationevent.NotificationEvent(fail_event))
+
+    def send_pushover(self, log_lines):
+        conn = http.client.HTTPSConnection("api.pushover.net:443")
+        conn.request("POST", "/1/messages.json",
+          urllib.parse.urlencode({
+            "token": self.config.pushover_app_token,
+            "user": self.config.pushover_user_key,
+            "message": log_lines,
+          }), { "Content-type": "application/x-www-form-urlencoded" })
+        conn.getresponse()
+
+    def send_email(self, log_lines):
+        post_result = requests.post(
+            "https://api.mailgun.net/v3/" + self.config.mailgun_domain_name + "/messages",
+            auth=("api", self.config.mailgun_api_key),
+            data={"from": "Marvin <mailgun@" + self.config.mailgun_domain_name + ">",
+                  "to": [self.config.email_address],
+                  "subject": "Marvin log",
+                  "text": log_lines})
+        if post_result.status_code == 200:
+            success_event = notificationevent.NotificationEventType.EMAIL_SENT
+            self.eventQueue.put(notificationevent.NotificationEvent(success_event))
+        else:
+            fail_event = notificationevent.NotificationEventType.EMAIL_FAILURE
+            self.eventQueue.put(notificationevent.NotificationEvent(fail_event))
 
     def join(self, timeout=None):
         self.stop_request.set()
